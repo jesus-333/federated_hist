d85ccb16b308f7a9bcfcea720e45d90403cf2be8
jesus <albi25.zanca@gmail.com>
2026-05-08 11:36:15 +0200

    Add file for future cli function

---
+++ src/clinnova_fl/cli.py
@@ -0,0 +1,14 @@
+"""
+Implements the command-line interface for the clinnova_fl package.
+
+Authors
+-------
+Alberto Zancanaro <alberto.zancanaro@uni.lu>
+"""
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Imports
+
+from __future__ import annotations
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
