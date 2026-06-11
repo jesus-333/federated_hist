1f1468118f9765067c1d440d2d20f4f4633409ec
jesus <albi25.zanca@gmail.com>
2026-06-11 18:00:13 +0200

    Updaload example fed hist

---
+++ examples/fed_hist_1/app.py
@@ -0,0 +1,11 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Imports
+
+from clinnova_fl.apps import client, server
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+server_app = server.app
+
+client_app = client.app
+
